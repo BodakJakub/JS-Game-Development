5ece1293d842c883ecd314fd97badab3f16c246b
Jakub Bodak <bodakjakub@gmail.com>
2017-10-23 18:12:44 +0200

    Set up basic drawing canvas

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,8 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50); //defines init position of rect as well as dimension
+ctx.fillStyle = "#FF0000"; //defines the color of rect
+ctx.fill(); // actually fills the rect with the defined color
+ctx.closePath();
