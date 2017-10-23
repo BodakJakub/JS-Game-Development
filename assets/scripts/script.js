b7912753395ea44b277603a2ceba44e231ab21ec
Jakub Bodak <bodakjakub@gmail.com>
2017-10-23 18:47:21 +0200

    Introduced ball and its movement to the canvas

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,19 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50); //defines init position of rect as well as dimension
-ctx.fillStyle = "#FF0000"; //defines the color of rect
-ctx.fill(); // actually fills the rect with the defined color
-ctx.closePath();
+setInterval(draw, 10);
+
+var x = canvas.width/2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+
+function draw() {
+	ctx.beginPath();
+	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+	x += dx;
+	y += dy;
+}
